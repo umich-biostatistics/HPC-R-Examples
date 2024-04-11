6afe02067976295e379c3e213ce565ddb9862d0d
Jacob Gladfelter <59100743+um-jglad@users.noreply.github.com>
2024-04-11 13:19:22 -0400

    fixed first sbatch command now that symlink is in place

--- R_Job_Arrays/Combining_Array_Data/runWorkload.sh
+++ R_Job_Arrays/Combining_Array_Data/runWorkload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-array=$(sbatch ../Saving_Array_Data/savearray.slurm | awk '{print $NF}')
+array=$(sbatch savearray.slurm | awk '{print $NF}')
 sbatch --dependency=afterok:$array combine.slurm
